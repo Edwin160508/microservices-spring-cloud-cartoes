7176b1d244c873e71e912fa1001174a342e4ad67
Edwin Lima <edwin.limaa@gmail.com>
2023-07-27 14:18:22 -0300

    incluindo logs pontuais.

--- projetos/mscartoes/src/main/java/io/github/edwinlima/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ projetos/mscartoes/src/main/java/io/github/edwinlima/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -7,12 +7,14 @@ import io.github.edwinlima.mscartoes.domain.DadosSolicitacaoEmissaoCartao;
 import io.github.edwinlima.mscartoes.infra.CartaoRepository;
 import io.github.edwinlima.mscartoes.infra.ClienteCartaoRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class EmissaoCartaoSubscriber {
 
     private final CartaoRepository cartaoRepository;
@@ -22,16 +24,18 @@ public class EmissaoCartaoSubscriber {
         try {
             var mapper = new ObjectMapper();
             DadosSolicitacaoEmissaoCartao dados = mapper.readValue(payload, DadosSolicitacaoEmissaoCartao.class);
+            log.info("Buscando cartao.");
             var cartaoEntity = cartaoRepository.findById(dados.getIdCartao()).orElseThrow();
 
             ClienteCartao clienteCartao = new ClienteCartao();
             clienteCartao.setCartao(cartaoEntity);
             clienteCartao.setCpf(dados.getCpf());
             clienteCartao.setLimite(dados.getLimiteLiberado());
-
+            log.info("Salvando cliente.");
             clienteCartaoRepository.save(clienteCartao);
         }catch (Exception e){
-            e.printStackTrace();
+            log.error("Erro ao receber solicitação de cartão: {}", e.getMessage());
+
         }
     }
 }
